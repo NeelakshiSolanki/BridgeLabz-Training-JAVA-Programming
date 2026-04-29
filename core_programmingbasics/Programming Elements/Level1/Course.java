7950466bcd9b1ff7a7ec7c4da58b4e31183b0230
Neelakshi solanki <122295718+neelakshi-204@users.noreply.github.com>
2026-04-29 16:00:58 +0530

    Course

---
+++ core_programmingbasics/Programming Elements/Level1/Course.java
@@ -0,0 +1,8 @@
+public class Course{
+    public static void main(String[] args){
+        int fee = 125000;
+        int discountPercent = 10;
+        int discounts = 125000 % 10;
+        
+    }
+}
