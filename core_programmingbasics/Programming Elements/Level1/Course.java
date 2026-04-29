f47d6de82745b21a36e49a56d7d9f910f60979d1
Neelakshi solanki <122295718+neelakshi-204@users.noreply.github.com>
2026-04-29 22:26:31 +0530

    Course

--- core_programmingbasics/Programming Elements/Level1/Course.java
+++ core_programmingbasics/Programming Elements/Level1/Course.java
@@ -1,8 +1,14 @@
-public class Course{
-    public static void main(String[] args){
+public class Course {
+    public static void main(String[] args) {
+
         int fee = 125000;
         int discountPercent = 10;
-        int discounts = 125000 % 10;
-        
+
+        double discount = (fee * discountPercent) / 100.0;
+        double finalFee = fee - discount;
+
+        System.out.println("Original Fee: INR " + fee);
+        System.out.println("Discount: INR " + discount);
+        System.out.println("Final Fee to Pay: INR " + finalFee);
     }
 }
