672a2fc0c262004b3b55d330f4c9e8800e9f0996
Neelakshi solanki <122295718+neelakshi-204@users.noreply.github.com>
2026-05-04 16:14:37 +0530

    Handshake

---
+++ core_programmingbasics/Programming Elements/Level1/Handshake.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Handshake {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+       
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+
+      
+        int handshakes = (n * (n - 1)) / 2;
+
+       
+        System.out.println("Maximum number of handshakes: " + handshakes);
+
+      
+    }
+}
