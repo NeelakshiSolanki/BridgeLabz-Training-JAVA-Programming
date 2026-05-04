191d1589d52a135b1ea9340ecb5d06546f71e03f
Neelakshi solanki <122295718+neelakshi-204@users.noreply.github.com>
2026-05-05 01:17:16 +0530

    FactorsWhile

---
+++ Core programming intermediate/Control Flow/Level 2/FactorsWhile.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class FactorsWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if (n > 0) {
+            int i = 1;
+
+            while (i <= n) {
+                if (n % i == 0)
+                    System.out.println(i);
+                i++;
+            }
+        } else {
+            System.out.println("Enter positive number");
+        }
+
+        
+    }
+}
