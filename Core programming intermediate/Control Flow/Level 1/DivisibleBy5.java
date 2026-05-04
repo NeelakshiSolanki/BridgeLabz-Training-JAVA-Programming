7270a95c2f5c735e1a27ee591d6e0b40e24d6470
Neelakshi solanki <122295718+neelakshi-204@users.noreply.github.com>
2026-05-04 16:30:10 +0530

    DivisibleBy5

---
+++ Core programming intermediate/Control Flow/Level 1/DivisibleBy5.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class DivisibleBy5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
+
+        boolean result = (num % 5 == 0);
+
+        System.out.println("Is the number " + num + " divisible by 5? " + result);
+
+
+    }
+}
