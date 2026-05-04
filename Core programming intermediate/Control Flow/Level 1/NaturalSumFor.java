075ec6f0a57114971cc4168eb69a9391c88ffd0b
Neelakshi solanki <122295718+neelakshi-204@users.noreply.github.com>
2026-05-04 20:26:03 +0530

    NaturalSumFor

---
+++ Core programming intermediate/Control Flow/Level 1/NaturalSumFor.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class NaturalSumFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if (n > 0) {
+            int sumFor = 0;
+
+            for (int i = 1; i <= n; i++) {
+                sumFor += i;
+            }
+
+            int sumFormula = (n * (n + 1)) / 2;
+
+            System.out.println("For Loop Sum = " + sumFor);
+            System.out.println("Formula Sum = " + sumFormula);
+
+            if (sumFor == sumFormula)
+                System.out.println("Both results are correct");
+        } else {
+            System.out.println("Not a natural number");
+        }
+
+      
+    }
+}
