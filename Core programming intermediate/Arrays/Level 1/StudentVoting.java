12fde1d0c14e3e9a59b57eaddadcef8daeae4e12
Neelakshi solanki <122295718+neelakshi-204@users.noreply.github.com>
2026-05-05 10:42:55 +0530

    StudentVoting

---
+++ Core programming intermediate/Arrays/Level 1/StudentVoting.java
@@ -0,0 +1,57 @@
+// Program Name: StudentVotingEligibility
+// Description: This program checks whether students can vote based on age
+// Author: Neelakshi
+
+import java.util.Scanner;
+
+class StudentVotingEligibility {
+
+    public static void main(String[] args) {
+
+      
+        Scanner scanner = new Scanner(System.in);
+
+        int totalStudents = 10;
+
+        
+        int[] studentAges = new int[totalStudents];
+
+        for (int i = 0; i < studentAges.length; i++) {
+
+            System.out.print("Enter age of student " + (i + 1) + ": ");
+
+            
+            if (scanner.hasNextInt()) {
+                studentAges[i] = scanner.nextInt();
+            } else {
+                System.out.println("Error: Invalid input. Please enter an integer.");
+                scanner.next(); 
+                i--;
+                continue;
+            }
+        }
+
+        System.out.println("\nVoting Eligibility");
+
+       
+        for (int i = 0; i < studentAges.length; i++) {
+
+            int age = studentAges[i];
+
+     
+            if (age < 0) {
+                System.out.println("Invalid age: " + age);
+            }
+
+            else if (age >= 18) {
+                System.out.println("Student with age " + age + " can vote.");
+            }
+            
+            else {
+                System.out.println("Student with age " + age + " cannot vote.");
+            }
+        }
+
+        
+    }
+}
