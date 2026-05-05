9215e946b59bbfab3e34a0b3d222ee29564d445d
Neelakshi solanki <122295718+neelakshi-204@users.noreply.github.com>
2026-05-05 10:49:14 +0530

    NumberAnalysis

---
+++ Core programming intermediate/Arrays/Level 1/NumberAnalysis.java
@@ -0,0 +1,77 @@
+// Program Name: NumberAnalysis
+// Description: This program checks numbers as positive/negative/zero,
+// even/odd (if positive), and compares first and last elements
+// Author: Neelakshi
+
+import java.util.Scanner;
+
+class NumberAnalysis {
+
+    public static void main(String[] args) {
+
+        // Creating Scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        // Constant for array size
+        int totalNumbers = 5;
+
+        // Array declaration
+        int[] numbers = new int[totalNumbers];
+
+        // Taking input with validation
+        for (int i = 0; i < numbers.length; i++) {
+
+            System.out.print("Enter number " + (i + 1) + ": ");
+
+            if (scanner.hasNextInt()) {
+                numbers[i] = scanner.nextInt();
+            } else {
+                System.out.println("Error: Please enter a valid integer.");
+                scanner.next(); 
+                i--;
+                continue;
+            }
+        }
+
+        System.out.println("\n Number Analysis");
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            int currentNumber = numbers[i];
+
+    
+            if (currentNumber > 0) {
+
+                if (currentNumber % 2 == 0) {
+                    System.out.println(currentNumber + " is positive and even.");
+                } else {
+                    System.out.println(currentNumber + " is positive and odd.");
+                }
+
+            }
+       
+            else if (currentNumber < 0) {
+                System.out.println(currentNumber + " is negative.");
+            }
+   
+            else {
+                System.out.println(currentNumber + " is zero.");
+            }
+        }
+
+        // Comparing first and last element
+        int firstElement = numbers[0];
+        int lastElement = numbers[numbers.length - 1];
+
+        System.out.println("\nComparison");
+
+        if (firstElement == lastElement) {
+            System.out.println("First and last elements are equal.");
+        } else if (firstElement > lastElement) {
+            System.out.println("First element is greater than last element.");
+        } else {
+            System.out.println("First element is less than last element.");
+        }
+
+    }
+}
--- Core programming intermediate/Arrays/Level 1/StudentVoting.java
+++ Core programming intermediate/Arrays/Level 1/StudentVoting.java
@@ -4,7 +4,7 @@
 
 import java.util.Scanner;
 
-class StudentVotingEligibility {
+class StudentVoting {
 
     public static void main(String[] args) {
 
